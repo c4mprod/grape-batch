fff0e2c4ab4d0304d8dffd20f4392180c4a65cb4
vfalduto <vincent.falduto@c4mprod.com>
2014-11-12 16:17:46 +0100

    [Change] Remove useless call to MultiJson.encode

--- lib/grape/batch.rb
+++ lib/grape/batch.rb
@@ -53,7 +53,7 @@ module Grape
           request_env['REQUEST_METHOD'] = method
           request_env['PATH_INFO'] = path
           if method == 'GET'
-            request_env['rack.input'] = StringIO.new(MultiJson.encode({}))
+            request_env['rack.input'] = StringIO.new('{}')
             request_env['QUERY_STRING'] = URI.encode_www_form(body.to_a)
           else
             request_env['rack.input'] = StringIO.new(MultiJson.encode(body))
