535f690a66d5c7e4f1cc8043bbdc23c00a5ca806
vfalduto <vincent.falduto@c4mprod.com>
2015-02-16 17:53:44 +0100

    [feature] Added log with notification and subscriber

---
+++ lib/grape/batch/log_subscriber.rb
@@ -0,0 +1,59 @@
+module Grape
+  module Batch
+    class LogSubscriber < ActiveSupport::LogSubscriber
+      def dispatch(event)
+        requests = event.payload[:requests]
+
+        if logger.debug?
+          debug_log(requests)
+        else
+          info_log(requests)
+        end
+      end
+
+      private
+
+      def debug_log(requests)
+        logger.info 'grape/batch'
+
+        requests.map do |params|
+          request, response = params
+          logger.info " method=#{request['REQUEST_METHOD']} path=#{request['PATH_INFO']}"
+
+          if request['REQUEST_METHOD'] == 'GET'
+            unless request['QUERY_STRING'].empty?
+              logger.debug "  params: #{request['QUERY_STRING'].to_s}"
+            end
+          else
+            if request['rack.input'].respond_to? :string
+              logger.debug "  body: #{request['rack.input'].string}"
+            end
+          end
+          logger.debug "  response: #{response.to_s}"
+        end
+      end
+
+      def info_log(requests)
+        messages = []
+        requests.each do |params|
+          request, response = params
+          messages << "method=#{request['REQUEST_METHOD']} path=#{request['PATH_INFO']}"
+        end
+
+        logger.info 'grape/batch ' + messages.join(', ')
+      end
+
+      def logger
+        @logger ||= Grape::Batch.configuration.logger || default_logger
+      end
+
+      def default_logger
+        logger = Logger.new($stdout)
+        logger.level = Logger::INFO
+        logger
+      end
+    end
+  end
+end
+
+Grape::Batch::LogSubscriber.attach_to(:batch)
--- spec/requests_spec.rb
+++ spec/requests_spec.rb
@@ -5,6 +5,10 @@ require 'grape'
 require 'api'
 
 RSpec.describe Grape::Batch::Base do
+  before(:all) do
+    Grape::Batch.configuration.logger = Logger.new('/dev/null')
+  end
+
   before :context do
     @app = Twitter::API.new
   end
@@ -31,15 +35,14 @@ RSpec.describe Grape::Batch::Base do
 
     describe 'GET /failure' do
       let(:response) { request.get('/api/v1/failure') }
-
       it { expect(response.status).to eq(503) }
-      it { expect(response.body).to eq(encode({error: 'Failed as expected'})) }
+      it { expect(response.body).to eq(encode({ error: 'Failed as expected' })) }
     end
   end
 
   describe '/batch' do
     let(:request_body) { nil }
-    let(:response) { request.post('/batch', {'CONTENT_TYPE' => 'application/json', input: request_body}) }
+    let(:response) { request.post('/batch', { 'CONTENT_TYPE' => 'application/json', input: request_body }) }
 
     context 'with invalid body' do
       it { expect(response.status).to eq(400) }
@@ -74,40 +77,40 @@ RSpec.describe Grape::Batch::Base do
       end
 
       context "when body['requests'] is not an array" do
-        let(:request_body) { encode({requests: 'request'}) }
+        let(:request_body) { encode({ requests: 'request' }) }
         it { expect(response.body).to eq("'requests' is not well formatted") }
       end
 
       context 'when request limit is exceeded' do
-        let(:request_body) { encode({requests: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}) }
+        let(:request_body) { encode({ requests: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] }) }
         it { expect(response.body).to eq('Batch requests limit exceeded') }
       end
 
       describe 'method attribute in request object' do
         context 'method is missing' do
-          let(:request_body) { encode({requests: [{}]}) }
+          let(:request_body) { encode({ requests: [{}] }) }
           it { expect(response.body).to eq("'method' is missing in one of request objects") }
         end
 
         context 'method is not a String' do
-          let(:request_body) { encode({requests: [{method: true}]}) }
+          let(:request_body) { encode({ requests: [{ method: true }] }) }
           it { expect(response.body).to eq("'method' is invalid in one of request objects") }
         end
 
         context 'method is invalid' do
-          let(:request_body) { encode({requests: [{method: 'TRACE'}]}) }
+          let(:request_body) { encode({ requests: [{ method: 'TRACE' }] }) }
           it { expect(response.body).to eq("'method' is invalid in one of request objects") }
         end
       end
 
       describe 'path attribute in request object' do
         context 'path is missing' do
-          let(:request_body) { encode({requests: [{method: 'GET'}]}) }
+          let(:request_body) { encode({ requests: [{ method: 'GET' }] }) }
           it { expect(response.body).to eq("'path' is missing in one of request objects") }
         end
 
         context 'path is not a String' do
-          let(:request_body) { encode({requests: [{method: 'GET', path: 123}]}) }
+          let(:request_body) { encode({ requests: [{ method: 'GET', path: 123 }] }) }
           it { expect(response.body).to eq("'path' is invalid in one of request objects") }
         end
       end
@@ -115,47 +118,47 @@ RSpec.describe Grape::Batch::Base do
 
     describe 'GET' do
       context 'with no parameters' do
-        let(:request_body) { encode({requests: [{method: 'GET', path: '/api/v1/hello'}]}) }
+        let(:request_body) { encode({ requests: [{ method: 'GET', path: '/api/v1/hello' }] }) }
         it { expect(response.status).to eq(200) }
-        it { expect(response.body).to eq(encode([{success: 'world'}])) }
+        it { expect(response.body).to eq(encode([{ success: 'world' }])) }
       end
 
       context 'with parameters' do
-        let(:request_body) { encode({requests: [{method: 'GET', path: '/api/v1/user/856'}]}) }
+        let(:request_body) { encode({ requests: [{ method: 'GET', path: '/api/v1/user/856' }] }) }
         it { expect(response.status).to eq(200) }
-        it { expect(response.body).to eq(encode([{success: 'user 856'}])) }
+        it { expect(response.body).to eq(encode([{ success: 'user 856' }])) }
       end
 
       context 'with a body' do
-        let(:request_body) { encode({requests: [{method: 'GET', path: '/api/v1/status', body: {id: 856}}]}) }
+        let(:request_body) { encode({ requests: [{ method: 'GET', path: '/api/v1/status', body: { id: 856 } }] }) }
         it { expect(response.status).to eq(200) }
-        it { expect(response.body).to eq(encode([{success: 'status 856'}])) }
+        it { expect(response.body).to eq(encode([{ success: 'status 856' }])) }
       end
 
       describe '404 errors' do
-        let(:request_body) { encode({requests: [{method: 'GET', path: '/api/v1/unknown'}]}) }
+        let(:request_body) { encode({ requests: [{ method: 'GET', path: '/api/v1/unknown' }] }) }
         it { expect(response.status).to eq(200) }
-        it { expect(response.body).to eq(encode([{code: 404, error: '/api/v1/unknown not found'}])) }
+        it { expect(response.body).to eq(encode([{ code: 404, error: '/api/v1/unknown not found' }])) }
       end
     end
 
     describe 'POST' do
       context 'with no parameters' do
-        let(:request_body) { encode({requests: [{method: 'POST', path: '/api/v1/hello'}]}) }
+        let(:request_body) { encode({ requests: [{ method: 'POST', path: '/api/v1/hello' }] }) }
         it { expect(response.status).to eq(200) }
-        it { expect(response.body).to eq(encode([{success: 'world'}])) }
+        it { expect(response.body).to eq(encode([{ success: 'world' }])) }
       end
 
       context 'with a body' do
-        let(:request_body) { encode({requests: [{method: 'POST', path: '/api/v1/status', body: {id: 856}}]}) }
+        let(:request_body) { encode({ requests: [{ method: 'POST', path: '/api/v1/status', body: { id: 856 } }] }) }
         it { expect(response.status).to eq(200) }
-        it { expect(response.body).to eq(encode([{success: 'status 856'}])) }
+        it { expect(response.body).to eq(encode([{ success: 'status 856' }])) }
       end
     end
 
     describe 'POST' do
       context 'with multiple requests' do
-        let(:request_body) { encode({requests: [{method: 'POST', path: '/api/v1/hello'}, {method: 'GET', path: '/api/v1/user/856'}]}) }
+        let(:request_body) { encode({ requests: [{ method: 'POST', path: '/api/v1/hello' }, { method: 'GET', path: '/api/v1/user/856' }] }) }
         it { expect(response.status).to eq(200) }
         it { expect(decode(response.body).size).to eq(2) }
       end
@@ -173,7 +176,7 @@ RSpec.describe Grape::Batch::Base do
 
     describe '.configure' do
       before do
-        allow(  Grape::Batch).to receive(:configuration) do
+        allow(Grape::Batch).to receive(:configuration) do
           config = Grape::Batch::Configuration.new
           config.path = '/custom_path'
           config.limit = 15
